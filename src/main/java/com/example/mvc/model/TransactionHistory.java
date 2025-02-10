109ed6caae486de68d3f2b25b44e288429ba1940
S.NIVEDHA <89909984+nive1712@users.noreply.github.com>
2025-02-10 18:15:20 +0530

    Create TransactionHistory.java

---
+++ src/main/java/com/example/mvc/model/TransactionHistory.java
@@ -0,0 +1,89 @@
+package com.example.mvc.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "transaction")
+public class TransactionHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id")
+    private int transactionId;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", referencedColumnName = "accountId")     
+    
+    private BankAccount bankAccount;
+    
+    @Column(name = "amount", precision = 15, scale = 2)
+    private BigDecimal amount;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "description", length = 500)
+    private String description;
+    public TransactionHistory() {}
+
+    public TransactionHistory(BankAccount bankAccount, BigDecimal amount, LocalDate date, String type, String description) {
+        this.bankAccount = bankAccount;
+        this.amount = amount;
+        this.date = date;
+        this.type = type;
+        this.description = description;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public BankAccount getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
